d2a78df7a2d6180b19eef9d85acc35ef7899d761
Muhammad Aqilul Muttaqin <aqilulmuttaqin16@gmail.com>
2024-10-10 16:49:44 +0700

    porotfolio -  experience (teknologi i've)

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,9 +9,14 @@ module.exports = {
           '0%, 100%': { borderRadius: '50% 30% 50% 30%' },
           '50%': { borderRadius: '30% 50% 30% 50%' },
         },
+        'loop-scroll': {
+          from: { transform: 'translateX(0)' },
+          to: { transform: 'translateX(-100%)' },
+        }
       },
       animation: {
         uneven: 'unevenBorderRadius 5s infinite ease-in-out',
+        'loop-scroll': 'loop-scroll 50s linear infinite',
       },
     },
   },
