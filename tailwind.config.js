6ea45c61bc3eb13cf357ddeca5c3d3a4af3a6e50
Muhammad Aqilul Muttaqin <aqilulmuttaqin16@gmail.com>
2024-10-11 16:38:42 +0700

    tambah animasi pada gambar dan perbaikan  z-index navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,18 +5,42 @@ module.exports = {
   theme: {
     extend: {
       keyframes: {
-        unevenBorderRadius: {
+        'uneven-border-radius': {
           '0%, 100%': { borderRadius: '50% 30% 50% 30%' },
           '50%': { borderRadius: '30% 50% 30% 50%' },
         },
-        'loop-scroll': {
-          from: { transform: 'translateX(0)' },
-          to: { transform: 'translateX(-100%)' },
-        }
+        'bounce-custom': {
+          '0%, 100%': { transform: 'translateY(0)' },
+          '50%': { transform: 'translateY(-10px)' },
+        },
+        'tilt-right': {
+          '0%, 100%': { transform: 'rotate(-15deg)' },
+          '25%': { transform: 'rotate(15deg)' },
+          '50%': { transform: 'rotate(-15deg)' },
+          '75%': { transform: 'rotate(15deg)' },
+        },
+        'tilt-left': {
+          '0%, 100%': { transform: 'rotate(15deg)' },
+          '25%': { transform: 'rotate(-15deg)' },
+          '50%': { transform: 'rotate(15deg)' },
+          '75%': { transform: 'rotate(-15deg)' },
+        },
+        'spin-custom': {
+          '0%': { transform: 'rotate(0deg)' },
+          '100%': { transform: 'rotate(360deg)' },
+        },
+        'zoom-in-out': {
+          '0%, 100%': { transform: 'scale(1)' },
+          '50%': { transform: 'scale(1.1)' },
+        },
       },
       animation: {
-        uneven: 'unevenBorderRadius 5s infinite ease-in-out',
-        'loop-scroll': 'loop-scroll 50s linear infinite',
+        'uneven-border-radius': 'uneven-border-radius 5s infinite ease-in-out',
+        'bounce-custom': 'bounce-custom 2s ease infinite',
+        'tilt-right': 'tilt-right 4s ease infinite',
+        'tilt-left': 'tilt-left 4s ease infinite',
+        'spin-custom': 'spin-custom 5s linear infinite',
+        'zoom-in-out': 'zoom-in-out 2s ease infinite',
       },
     },
   },
